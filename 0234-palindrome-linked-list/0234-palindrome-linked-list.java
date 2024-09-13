bc7e29149e72d1487ec60062512f4686fbcdf860
Main Monarchist 🎗 <divyanshvairagade@gmail.com>
2024-09-13 12:35:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode a = head.next;
+        ListNode newHead = reverse(a);
+        a.next = head;
+        head.next = null;
+        return newHead;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode j = reverse(slow);
+        ListNode i = head; 
+        while(j!=null){
+            if(i.val!=j.val) return false;
+            j = j.next;
+            i = i.next;
+        }
+        return true;
+    }
+}
