e480bfc880420a2d93a83aebf44625287ae3aa0c
Main Monarchist 🎗 <divyanshvairagade@gmail.com>
2024-09-13 12:36:08 +0530

    Time: 8 ms (32.85%), Space: 61.9 MB (60.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -20,7 +20,7 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast!=null){
+        while(fast!=null&&fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
